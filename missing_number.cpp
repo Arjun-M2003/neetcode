6f0d6e85db613b8628d9804caf9d0954836e1656
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-08-26 16:38:20 -0400

    finished problem

---
+++ missing_number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // xor cancels out any number seen twice
+        
+        int n = nums.size();
+        int xr = 0;
+
+        // introduce every number first
+        for(int i = 0; i <= n; i++){
+            xr ^= i;
+        }
+
+        // loop through nums to find the missing one
+        for(int num : nums){
+            xr ^= num;
+        }
+
+        return xr;
+    }
+};
