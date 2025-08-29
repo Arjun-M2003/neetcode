cea13e8bc4676eb0ceefb5ab095637fcf6823dba
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-08-29 15:31:29 -0400

    min stack implementation

---
+++ min_stack.cpp
@@ -0,0 +1,33 @@
+#include <stack>
+class MinStack {
+private:
+    std::stack<int> stack;
+    std::stack<int> minStack;
+
+public:
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        stack.push(val);
+        if(minStack.empty() || val <= minStack.top()){
+            minStack.push(val);
+        }
+    }
+    
+    void pop() {
+        if(stack.top() == minStack.top()){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    int top() {
+        return stack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
