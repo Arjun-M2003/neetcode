f5417481b7f338007ae4704ce34e002bc352d483
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-10-03 19:54:43 -0400

    finished problem

---
+++ partition_labels.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char, int> lastIndex;
+        for(int i = 0; i < s.size(); i++){
+            lastIndex[s[i]] = i; // stores last index for each character 
+        }
+
+        vector<int> output;
+
+        int start = 0, end = 0;
+        for(int i = 0; i < s.size(); i++){
+            end = max(end, lastIndex[s[i]]);
+
+            if(i == end){
+                output.push_back(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return output;
+    }
+};
